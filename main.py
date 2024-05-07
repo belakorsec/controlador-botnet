61c0da37d4155e93ce0f7a8121280d8e783b0f18
belakorsec <165942832+belakorsec@users.noreply.github.com>
2024-05-06 19:14:12 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,20 +1,18 @@
-from flask import Flask, Response
+import flask
 
-app = Flask(__name__)
+app = flask.Flask(__name__)
 
 
-@app.route("/")
-def prueba():
+@app.route("/", methods=["GET"])
+def send_data():
+    input_data = int(input("Total requests: "))
     data = {
-        "signal": False,
-        "payload": {
-            "total_requests": 90,
-            "concurrent_requests": 10,
-            "method": "GET",
-            "target_url": "http://hola.com",
-            "wait_time": 5,
-            "timeout": 30,
-        },
+        "total_requests": input_data,
+        "concurrent_requests": 10,
+        "method": "GET",
+        "target_url": "https://www.hola.com",
+        "wait_time": 5,
+        "timeout": 30,
     }
     return data
 
